c54a3258e9ab3747a0f67507932ab43e20a93d22
Don Thorp <don@donthorp.net>
2022-01-07 15:09:28 -0600

    Initial nvim configuration

---
+++ home/private_dot_config/nvim/init.lua
@@ -0,0 +1,26 @@
+-- Visual Settings
+
+local set = vim.opt
+
+set.syntax = 'on'
+set.background = 'light'
+set.number = true
+
+-- Encoding
+
+set.encoding = "utf-8"
+set.fileencoding = "utf-8"
+set.fileencodings = "utf-8"
+set.ttyfast = true
+
+-- Fix backspace indent
+
+set.backspace = "indent,eol,start"
+
+-- Tabs
+
+set.tabstop=4
+set.softtabstop = 0
+set.shiftwidth = 4
+set.expandtab = true
+
