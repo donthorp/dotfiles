c44184680dddf1250c0f8fbd6af4dbb4fb3f24cd
Don Thorp <don@donthorp.net>
2022-03-17 08:18:55 -0500

    Switching to a different style of init
    Former-commit-id: 9b76cdfa3ace5b9659defba02d22f94ba6e76286

--- home/private_dot_config/nvim/init.lua
+++ home/private_dot_config/nvim/init.lua
@@ -1,26 +1,4 @@
--- Visual Settings
-
-local set = vim.opt
-
-set.syntax = 'on'
-set.background = 'light'
-set.number = true
-
--- Encoding
-
-set.encoding = "utf-8"
-set.fileencoding = "utf-8"
-set.fileencodings = "utf-8"
-set.ttyfast = true
-
--- Fix backspace indent
-
-set.backspace = "indent,eol,start"
-
--- Tabs
-
-set.tabstop=4
-set.softtabstop = 0
-set.shiftwidth = 4
-set.expandtab = true
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
 
