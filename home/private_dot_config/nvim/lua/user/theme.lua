e49a5fc3f8350da5d353eb90fec017c0d4d74672
Don Thorp <don@donthorp.net>
2022-03-27 12:27:27 -0500

    Neovide theme tweaks

--- home/private_dot_config/nvim/lua/user/theme.lua
+++ home/private_dot_config/nvim/lua/user/theme.lua
@@ -1,11 +1,14 @@
+-- Configuring appearance
+
 
 vim.cmd [[
-
-  set guifont=FiraCode-Regular:h15
+  set guifont=Fira\ Code:h13
   colorscheme zenbones
 ]]
 
-vim.g.zenbones = #{
+vim.g.zenbones = {
   lightness = 'bright',
-  darken_concurrent_window = true
+  darken_noncurrent_window = true,
+  darken_comments = 80,
+  darken_non_text = 30,
 }
